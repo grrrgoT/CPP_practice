94918a23e814543e6446f707209a2a88361c3784
Go Terauchi <goterauchi5@gmail.com>
2026-08-01 23:44:34 +1000

    1.1 project

---
+++ 1.1_project.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    int Favorite_number;
+    std::cout << "Enter your favorite number between 1 and 100: ";
+    std::cin >> Favorite_number;
+    std::cout << "Amazing!! That's my favorite number too!" << std::endl;
+    std::cout << "No really!!, " << Favorite_number << " is my favorite number!" << std::endl;
+
+    return 0;
+}
